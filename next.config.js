66983c86890d8346c5b2a7d7c8a3227e5f0a55c1
Mikael Ramdeen <mikael.ramdeen@hotmail.com>
2022-11-23 10:28:02 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i.ytimg.com', 'lh3.googleusercontent.com']
+  }
 }
 
 module.exports = nextConfig
